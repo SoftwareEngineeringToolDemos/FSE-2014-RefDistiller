8b1cea52348ca44379383a8bd7b6fa8e48270505
apoorvmahajan <apoorv.mahajan09@gmail.com>
2015-11-28 17:15:11 -0500

    Updated Project path

--- build-vm/commands.sh
+++ build-vm/commands.sh
@@ -2,5 +2,5 @@
 
 cd Desktop/eclipse
 sudo chmod +rwx eclipse
-./eclipse -import /home/vagrant/Desktop/RefDistillerWorkspace/OriginalVersion/ -import home/vagrant/Desktop/RefDistillerWorkspace/Refactored/
+./eclipse -import /home/vagrant/Desktop/RefDistillerWorkspace/OriginalVersion/ -import /home/vagrant/Desktop/RefDistillerWorkspace/Refactored/
 
