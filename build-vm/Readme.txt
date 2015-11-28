95e7cf9fd58b0a440672dc0bdb1d55a7662f796d
apoorvmahajan <apoorv.mahajan09@gmail.com>
2015-11-28 16:36:23 -0500

    Adding VM contents

---
+++ build-vm/Readme.txt
@@ -0,0 +1,8 @@
+This tool is the work of: Dr.Everton L. G. Alves, Dr. Myoungkyu Song, Dr.Miryung Kim
+The paper for this tool can be accessed from : http://dl.acm.org/citation.cfm?id=2661674
+The demo video made by the authors for the tool can be accessed from :https://www.youtube.com/watch?v=0Iseoc5HRpU&feature=youtu.be
+________________________________________________________________________________________________________________________________________________
+
+
+
+
--- build-vm/commands.sh
+++ build-vm/commands.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-cd Desktop
-
- # install eclipse mars
-   wget http://mirror.cc.columbia.edu/pub/software/eclipse/technology/epp/downloads/release/mars/1/eclipse-jee-mars-1-linux-gtk-x86_64.tar.gz
-
-#extract eclipse
-   tar xvzf eclipse-jee-mars-1-linux-gtk-x86_64.tar.gz
-
-  cd eclipse/plugins
-
-   wget https://github.com/SoftwareEngineeringToolDemos/FSE-2014-RefDistiller/raw/master/Binary/refDistiller.tar.gz
-
-# install RefDistiller plugin
-  tar xvzf refDistiller.tar.gz
-
-cp refDistiller.tar.gz /home/vagrant/Desktop
-
-cd ..
-
+cd Desktop/eclipse
 sudo chmod +rwx eclipse
-
-./eclipse
+./eclipse -import /home/vagrant/Desktop/RefDistillerWorkspace/OriginalVersion/ -import home/vagrant/Desktop/RefDistillerWorkspace/Refactored/
 
